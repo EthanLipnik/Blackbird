2da048c08f0bd58bcd13cd0db8c78e7511748254
Ethan Lipnik <ethan@tailosive.net>
2019-10-05 15:49:14 -0400

    Added proper error handling

--- Sources/Foto/ImageDetails.swift
+++ Sources/Foto/ImageDetails.swift
@@ -12,23 +12,23 @@ import UIKit
 extension UIImage {
 	
 	public func histogram() -> UIImage? {
-		guard let beginImage = CIImage(image: self) else { return nil }
+		guard let beginImage = CIImage(image: self) else { print("No image to proccess"); return nil }
 		
-		guard let filter = CIFilter(name: "CIAreaHistogram") else { return nil }
+		guard let filter = CIFilter(name: "CIAreaHistogram") else { print("Failed to create the histogram filter"); return nil }
 		filter.setValue(beginImage, forKey: kCIInputImageKey)
 		filter.setValue(256, forKey: "inputCount")
 		filter.setValue(beginImage.extent, forKey: "inputExtent")
 		filter.setValue(1.0, forKey: "inputScale")
 		
-		guard let histogramData = filter.outputImage else { return nil }
+		guard let histogramData = filter.outputImage else { print("Failed to get histogram data"); return nil }
 		
-		guard let histogramFilter = CIFilter(name: "CIHistogramDisplayFilter") else { return nil }
+		guard let histogramFilter = CIFilter(name: "CIHistogramDisplayFilter") else { print("Failed to create histogram display filter"); return nil }
 		histogramFilter.setValue(histogramData, forKey: kCIInputImageKey)
 		histogramFilter.setValue(200, forKey: "inputHeight")
 		histogramFilter.setValue(0, forKey: "inputHighLimit")
 		histogramFilter.setValue(0, forKey: "inputLowLimit")
 		
-		guard let finalHistogram = histogramFilter.outputImage else { return nil }
+		guard let finalHistogram = histogramFilter.outputImage else { print("Failed to get histogram output") return nil }
 		
 		return UIImage(ciImage: finalHistogram)
 	}
